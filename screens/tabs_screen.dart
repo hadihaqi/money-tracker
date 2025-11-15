9a545c0bb97ffbb2b31bd4fc4f2561a17b9f5cb8
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-15 13:52:37 +0330

    tabs screen created

---
+++ screens/tabs_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Tabs Screen'));
+  }
+}
