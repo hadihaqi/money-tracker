67ca53bad0014403a347eddb894c7e6c8c954479
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-19 14:31:01 +0330

    users dummy data added to users provider

--- providers/users.dart
+++ providers/users.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/legacy.dart';
+
 import 'package:money_tracker/models/user.dart';
+import 'package:money_tracker/dummy_data/users.dart';
 
 class UsersNotifier extends StateNotifier<List<User>> {
-  UsersNotifier() : super([]);
+  UsersNotifier() : super(dummyUsers);
 
   void addUser(User user) {
     state = [...state, user];
