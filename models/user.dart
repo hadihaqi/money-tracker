850ceaa908b78b480ae2b932c513eab03db4ead8
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-19 14:07:01 +0330

    id field added to user model class

--- models/user.dart
+++ models/user.dart
@@ -1,10 +1,12 @@
-class User {
-  User({
-    required this.fullName,
-    required this.email,
-    required this.password,
-  });
+import 'package:uuid/uuid.dart';
 
+const uuid = Uuid();
+
+class User {
+  User({required this.fullName, required this.email, required this.password})
+    : id = uuid.v4();
+
+  final String id;
   final String fullName;
   final String email;
   final String password;
