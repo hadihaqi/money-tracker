fc67e02c4fa6fc15b3203d41aea3a2d0d7755c4d
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-23 16:36:22 +0330

    current user provider is returning the first dummy user

--- providers/current_user.dart
+++ providers/current_user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:money_tracker/dummy_data/users.dart';
 import 'package:money_tracker/models/user.dart';
 
-final currentUserProvider = StateProvider<User?>((ref) => null);
+final currentUserProvider = StateProvider<User>((ref) => dummyUsers.first);
