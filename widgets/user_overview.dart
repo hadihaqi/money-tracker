e23878275199159af11e421e0fabbdad590bc9d5
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-25 16:27:34 +0330

    the amount of user_oview widget formatted

---
+++ widgets/user_overview.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:money_tracker/providers/current_user.dart';
+
+class UserOverview extends ConsumerWidget {
+  const UserOverview({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserProvider);
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Stack(
+        alignment: AlignmentGeometry.center,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.yellow[700],
+            ),
+          ),
+          Icon(CupertinoIcons.person_fill, color: Colors.yellow[800]),
+        ],
+      ),
+      title: Text(currentUser.fullName),
+      subtitle: Text(
+        currentUser.email,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
+          color: Theme.of(context).colorScheme.outline,
+        ),
+      ),
+      trailing: IconButton(
+        onPressed: () {},
+        icon: Icon(CupertinoIcons.settings),
+      ),
+    );
+  }
+}
