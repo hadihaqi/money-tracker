d6565d7edffa630bad203f01bed05ae34597b93e
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-25 16:54:41 +0330

    settings_item widget added

---
+++ widgets/settings_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsItem extends StatelessWidget {
+  SettingsItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.subtitle,
+  });
+
+  final IconData icon;
+  final String title;
+  String? subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return s
+  }
+}
