722959bd19c2342521a78a26d1bc09036587075d
Hadi Edalat <hadihaqi1377.he@gmail.com>
2025-11-26 16:02:44 +0330

    users_db_helper added

---
+++ db/users_db_helper.dart
@@ -0,0 +1,59 @@
+import 'package:money_tracker/models/user.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class UsersDbHelper {
+  static Database? _db;
+
+  static Future<Database> get database async {
+    if (_db != null) return _db!;
+
+    _db = await _initDB('users.db');
+    return _db!;
+  }
+
+  static Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  static Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users(
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+      )
+''');
+  }
+
+  // ------------ User Methods ------------
+
+  static Future<int> insertUser(User user) async {
+    final db = await database;
+    return await db.insert('users', user.toMap());
+  }
+
+  static Future<List<User>> getUsers() async {
+    final db = await database;
+    final result = await db.query('users');
+    return result.map((map) => User.fromMap(map)).toList();
+  }
+
+  static Future<int> deleteUser(int id) async {
+    final db = await database;
+    return await db.delete('users', where: 'id = ?', whereArgs: [id]);
+  }
+
+  static Future<int> updateUser(User user) async {
+    final db = await database;
+    return await db.update(
+      'users',
+      user.toMap(),
+      where: 'id = ?',
+      whereArgs: [user.id],
+    );
+  }
+}
